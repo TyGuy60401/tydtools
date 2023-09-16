6ee7f542860b750b323125dac10f12070c8cbcca
Ty Davis <tydavisxc2019@gmail.com>
2023-09-16 10:27:26 -0600

    Added eceLib and a simple gitignore

---
+++ eceLib.py
@@ -0,0 +1,74 @@
+import sys
+
+def volt_divide(vin: float, rs: list):
+    """Return a list containing the voltages across each resistor in the list rs
+    
+    Parameters
+    ----------
+    vin:    Voltage to be divided
+    rs: List of resistors that are dividing the voltage vin
+    
+    Returns
+    -------
+    vs: List of divided voltages corresponding to the resistors in rs
+    """
+    rtot = sum(rs)
+    vs = []
+    for r in rs:
+        vs.append(vin * r / rtot)
+    return vs
+    
+
+
+def calc2Para(res1=float, res2=float):
+    """
+    Deprecated. Used to calculate the value of two 
+    resistors or inductors in parallel.
+    Use parallel() instead.
+    """
+    return (res1*res2)/(res1 + res2)
+
+def parallel(vList: list):
+    """Return the equivalent value of the  """
+    sum = 0
+    for val in vList:
+        sum += 1/val
+    return 1/sum
+
+
+def opampSum():
+    voltDict = {}
+    getInput = True
+    i = 0
+    Rf = float(input("Enter a resistance for Rf: "))
+
+    while getInput:
+        voltage = input("Enter a voltage (end for end) ")
+        if voltage == "end".lower():
+            getInput = False
+        else:
+            resistance = float(input("Enter a resistance: "))
+            voltage = float(voltage)
+            
+            dictString = str(i)
+            voltDict[dictString] = [voltage, resistance]
+            i += 1
+            print(voltDict)
+    
+    if len(voltDict) > 0:
+        sumInside = 0
+        for ii in range(len(voltDict)):
+            sumInside += voltDict[ii][0]/voltDict[ii][1]
+        ans = -Rf * (sumInside)
+        print("Answer is: ")
+        print(ans)
+        print("  ---  ")
+    
+
+if __name__ == '__main__':
+    # Testing the parallel function
+    vList = []
+    for arg in sys.argv[1:]:
+        vList.append(float(arg))
+    print(parallel(vList))
+
